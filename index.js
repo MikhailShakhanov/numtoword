ede74ef985629308d0636dd360d89d509142bf77
mi.shakhanov <mi.shakhanov@jet.su>
2022-02-13 22:07:05 +0300

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./server');
 const config = require('./config');
+const PORT = process.env.PORT || config.PORT
 
-server.listen(config.PORT, () => {
-  console.log(`Server is listening on http://localhost:${config.PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
